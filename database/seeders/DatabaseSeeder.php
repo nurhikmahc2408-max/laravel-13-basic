7da2ec46bb71a6644bf4866090b7234748e1a448
Nurhikmah Cahyani Ashari <nurhikmahc2408@gmail.com>
2026-04-29 16:59:55 +0800

    Pertemuan 5: Relationship

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
         $this->call([
-            StudentSeeder ::class,
+            StudentSeeder::class,
+            DepartmentSeeder::class,
+            LecturerSeeder::class,
         ]);
         
         
